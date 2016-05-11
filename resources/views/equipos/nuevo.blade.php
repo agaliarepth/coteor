c1cf1e662b9283f6c9430f158db27eff427528db
agaliarepth <vasquezelver@gmail.com>
2016-05-10 20:37:28 -0500

    segundo commit

--- resources/views/equipos/nuevo.blade.php
+++ resources/views/equipos/nuevo.blade.php
@@ -9,18 +9,41 @@
                 <section class="panel">
                     <div class="panel-body">
 
-
-                        <div class="col-md-12">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="codigo">Codigo</label>
+                            <input type="text" class="form-control" id="codigo" name="codigo" required  placeholder="codigo">
+                        </div>
+                    </div>
+                        <div class="col-md-8">
                             <div class="form-group">
                                 <label for="" >Descripcion</label>
-                                <input type="text" class="form-control" id="descripcion" required name="descripcion"placeholder="Descripcion categoria">
+                                <input type="text" class="form-control" id="descripcion" required name="descripcion"placeholder="Descripcion">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="" >Marca</label>
+                                <input type="text" class="form-control" id="marca"  name="marca" placeholder="marca">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="" >Modelo</label>
+                                <input type="text" class="form-control" id="modelo"  name="modelo" placeholder="modelo">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="" >Items</label>
+                                <input type="text" class="form-control" id="items_id"  name="items_id" placeholder="codigo item">
                             </div>
                         </div>
 
-                        <div class="col-md-4">
+                        <div class="col-md-12">
                             <div class="form-group">
-                                <label for="">Codigo</label>
-                                <input type="text" name="codigo" class="form-control" id="codigo" required  placeholder="Codigo"  value="">
+                                <label for="">Observacion</label>
+                                <textarea  name="observaciones" class="form-control" id="observaciones"  maxlength="255" value=""></textarea>
                             </div>
                         </div>
 
@@ -32,6 +55,7 @@
                 <input type="hidden" name='_token' value='{{csrf_token()}}' id="token">
                 <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fa fa-times"></i> Cerrar</button>
                 <button type="button" class="btn btn-success" onclick="guardar();"> <i class=" fa fa-floppy-o"></i> Guardar</button>
+
             </div>
 
         </div>
